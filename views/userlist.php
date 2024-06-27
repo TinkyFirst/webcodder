70dc11a84fa40dfec6815dd80835169d5ae9ad8f
andriirsee <vipoman.bk@gmail.com>
2024-06-27 15:44:01 +0300

    realize userlist page, user page

--- views/userlist.php
+++ views/userlist.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/path/to/bootstrap.css" rel="stylesheet">
+    <link href="" rel="stylesheet">
     <title>User List</title>
 </head>
 <body>
